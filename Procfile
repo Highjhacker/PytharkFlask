0455131a6f87e9a3f284ba8820ca4dfe7b4b6b20
jolan <mc-somango@live.fr>
2017-11-14 19:08:55 +0100

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run.py
+web: gunicorn manage:app
---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask_script import Manager, Shell
+import os
+
+from app import create_app
+
+app = create_app(os.getenv('APP_SETTINGS'))
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app)
+
+manager.add_command("shell", Shell(make_context=make_shell_context()))
+
+if __name__ == '__main__':
+    manager.run()
